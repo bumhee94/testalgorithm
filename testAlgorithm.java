fe4bf4d15995bfd1f2b8b43e845cbbcd52858e19
이범희 <bhlee94@naver.com>
2022-10-20 17:16:11 +0900

    짝수와 홀수

--- testAlgorithm.java
+++ testAlgorithm.java
@@ -7,4 +7,19 @@ class Solution {
     }
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/120807
+//https://school.programmers.co.kr/learn/courses/30/lessons/120807 
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        
+        if(num % 2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12937?language=java
